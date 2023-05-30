6248b538b03f5f9bcee9003feb220b13974342eb
Nelson Soares <86508086+nelsonsoares@users.noreply.github.com>
2023-05-30 18:14:31 -0300

    Correção de autenticação de usuário na tela login

--- App/controller/controllerLogin.php
+++ App/controller/controllerLogin.php
@@ -5,7 +5,7 @@ require_once '../model/bd_connect.php';
 $email = $_POST['email'];
 $senha  = $_POST['senha'];
 
-$sql = "SELECT * FROM clientes";
+$sql = "SELECT * FROM clientes WHERE email = '$email'";
 
 $resultadoConsulta = mysqli_query($connect, $sql);
 
@@ -16,7 +16,7 @@ $senha_db = $linha['senha'];
 
 $senhaVerificada = password_verify($senha, $senha_db);
 
-if ($email_db === $email and $senhaVerificada === true) {
+if ($email_db == $email and $senhaVerificada == 1) {
   header("Location: ../view/home.html");
 } else {
   header("Location: ../view/login.html");
