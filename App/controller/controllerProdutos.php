7ce9c9ce21f75cebffafe9a9d550c15f0982057d
Nelson Soares <86508086+nelsonsoares@users.noreply.github.com>
2023-05-30 17:31:54 -0300

    Correção de erro no move_upload_file e exclusão das pasta bd com os arquivos txt

--- App/controller/controllerProdutos.php
+++ App/controller/controllerProdutos.php
@@ -7,16 +7,21 @@ if (isset($_POST['cadastrarProduto'])) { // Verifica se houve o clique no botão
   $nome = $_POST['nomeProduto'];
   $descricao = $_POST['descricaoProduto'];
   $valor = (float) $_POST['valorProduto'];
-  $imagem = "../imgProduto/" . $_FILES['imgProduto']['name'];
+  $imgName = $_FILES['imgProduto']['name'];
 
   $formatosValidos = ["png", "jpg", "jpeg"];
+
   $extensao = pathinfo($_FILES['imgProduto']['name'], PATHINFO_EXTENSION); //Pega o nome da extensão da imagem
 
   if (in_array($extensao, $formatosValidos)) { //Verifica se a extensão é válida
 
-    $sql = "INSERT INTO produtos (nome, descricao, valor, imagem) VALUES ('$nome', '$descricao', '$valor', '$imagem')";
+    $sql = "INSERT INTO produtos (nome, descricao, valor, imagem) VALUES ('$nome', '$descricao', '$valor', '../imgProduto/$imgName')";
 
     if (mysqli_query($connect, $sql)) { // Envia a consulta para o banco de dados.
+
+      $tempPath = $_FILES['imgProduto']['tmp_name'];
+      $dirImgProduto = "../view/imgProduto/" . $_FILES['imgProduto']['name'];
+      move_uploaded_file($tempPath, $dirImgProduto); //Move a imagem para a pasta imgProduto
 
       print "<script>alert('Produto cadastrado com sucesso!!!')</script>";
       header("Location: ../view/cadastroProdutos.html"); //Redireciona para a página de Produtos
@@ -27,15 +32,9 @@ if (isset($_POST['cadastrarProduto'])) { // Verifica se houve o clique no botão
       header("Location: ../view/cadastroProdutos.html"); //Redireciona para a página de Produtos
 
     }
-
-    $tempPath = $_FILES['imgProduto']['tmp_name'];
-
-    move_uploaded_file($tempPath, $imagem); //Move a imagem para a pasta imgProduto
-
   } else {
 
-    print "<script>alert('Formato de arquivo inválido!!!')</script>";
-    
+    // print "<script>alert('Formato de arquivo inválido!!!')</script>";
   }
 }
 
